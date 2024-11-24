7d6e3623a485cb8d3f7c98f8cb40dd7f550a3cfd
codewithpraful <chaudharyprashant83663@gmail.com>
2024-11-24 10:24:24 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -2,14 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './Models/DB.js';
-import userRoutes from './Routes/UserRoutes.js';
+
+
+
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 await connectDB();
 app.use(express.json());
 app.use(cors());
-app.use('/api/user', userRoutes)
+
+
 app.get('/',(req,res)=>res.send("API Working"));
 app.listen(PORT,()=>console.log(`Server Running On PORT ${PORT}`));
 
